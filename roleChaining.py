22686bf865e6e1720a11eb402afa6fdd86f3aab2
AsifTra <asif.trabelsi97@gmail.com>
2024-09-09 20:19:47 +0300

    script update

--- roleChaining.py
+++ roleChaining.py
@@ -31,22 +31,22 @@ def authenticate_user(session):
         cprint(f'Error authenticating user:\n{e}', 'red')
         sys.exit(1)
 
-def get_assumable_roles(session, user_arn):
-    """List all account roles and which of those roles the user can assume."""
+def get_permisive_roles(session, user_arn):
+    """Check if the given user has permission on a given role."""
     client = session.client('iam')
     roles = []
     try:
         for page in client.get_paginator('list_roles').paginate():
             roles.extend(page['Roles'])
-        assumable_roles = [
+        permisive_roles = [
             {
                 'RoleName': role['RoleName'],
                 'RoleArn': role['Arn']
             }
             for role in roles
-            if get_assume_role_permission(role['AssumeRolePolicyDocument'], user_arn)
+            if get_role_permission(role['AssumeRolePolicyDocument'], user_arn)
         ]
-        return assumable_roles
+        return permisive_roles
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == 'AccessDenied':
             cprint(f"Access Denied: {e.response['Error']['Message']}", "red")
@@ -55,8 +55,8 @@ def get_assumable_roles(session, user_arn):
             cprint(f"An unexpected error occurred: {e.response['Error']['Message']}", "red")
             sys.exit(1)
 
-def get_assume_role_permission(policy_document, user_arn):
-    """Check if the given user has permission on a given role based on its policy document."""
+def get_role_permission(policy_document, user_arn):
+    """Helper to get_permisive_roles function, help parse the document policy."""
     for statement in policy_document.get('Statement', []):
         if statement.get('Effect') == 'Allow' and 'Principal' in statement:
             principal_arns = statement['Principal'].get('AWS', [])
@@ -66,27 +66,33 @@ def get_assume_role_permission(policy_document, user_arn):
                 return True
     return False
 
-def role_chaning_check(session, assumable_roles):
-    """For each assumable role, check both inline and managed policies to see if the role allows assuming another role."""
+def role_chaning_check(session, permisive_roles):
+    """For each permisive role, check both inline and managed policies to see if the role allows assuming another role."""
     client = session.client('iam')
-    for role in assumable_roles:
+    role_chaining_found = False
+    assumable_roles = []  # List to store all roles allowing role chaining
+
+    for role in permisive_roles:
         role_name = role['RoleName']
         role_arn = role['RoleArn']
 
-        check_policies(
-            session, role_name, role_arn, 
-            client.get_paginator('list_role_policies'), 
-            get_role_policy
-        )
+        if check_policies(session, role_name, role_arn, client.get_paginator('list_role_policies'), get_role_policy) or \
+           check_policies(session, role_name, role_arn, client.get_paginator('list_attached_role_policies'), get_managed_policy):
+            creds = assume_user_role(session, role_name, role_arn)
+            if creds:
+                assumable_roles.append(role_name)
+                role_chaining_found = True
+                cprint(f"Assumed role credentials from '{role_name}' role:", "green")
+                print(json.dumps(creds, default=convert_datetime, indent=2))
 
-        check_policies(
-            session, role_name, role_arn, 
-            client.get_paginator('list_attached_role_policies'), 
-            get_managed_policy
-        )
+    if not role_chaining_found:
+        cprint("No roles found that allow chaining.", "red")
+    else:
+        cprint(f"Roles that allow chaining: {', '.join(assumable_roles)}", "green")
 
 def check_policies(session, role_name, role_arn, paginator, policy_fetcher):
-    """Check if any policies (inline or managed) allow assuming another role."""
+    """Helper function to determine whether the policy (of each permisive role) is managed or inline."""
+    role_allows_assume_role = False
     for page in paginator.paginate(RoleName=role_name):
         for policy in page.get('PolicyNames', []) + page.get('AttachedPolicies', []):
             policy_document = policy_fetcher(session, role_name, policy)
@@ -94,30 +100,26 @@ def check_policies(session, role_name, role_arn, paginator, policy_fetcher):
                 policy_type = 'inline' if 'PolicyName' in policy else 'managed'
                 policy_name = policy if isinstance(policy, str) else policy['PolicyName']
                 cprint(f"{policy_type.capitalize()} policy '{policy_name}' in role '{role_name}' allows assuming another role.", "green")
-                creds = assume_user_role(session, role_name, role_arn)
-                if creds:
-                    cprint(f"Assumed role credentials from '{role_name}' role:", "green")
-                    print(json.dumps(creds, default=convert_datetime, indent=2))
+                role_allows_assume_role = True
+    return role_allows_assume_role
 
 def get_role_policy(session, role_name, policy_name):
-    """Fetch the policy document for an inline policy."""
+    """Helper function for check_policies function, returning the role policies of inline policies."""
     client = session.client('iam')
     return client.get_role_policy(RoleName=role_name, PolicyName=policy_name)['PolicyDocument']
 
 def get_managed_policy(session, role_name, policy):
-    """Fetch the policy document for a managed policy."""
+    """Helper function for check_policies function, returning the role policies of managed policies."""
     client = session.client('iam')
     policy_arn = policy['PolicyArn']
     policy_version = client.get_policy(PolicyArn=policy_arn)['Policy']['DefaultVersionId']
     return client.get_policy_version(PolicyArn=policy_arn, VersionId=policy_version)['PolicyVersion']['Document']
 
 def policy_allows_assume_role(policy_document):
-    """Check if a policy document allows assuming a role."""
+    """Helper function for check_policies function, Check if a policy document allows assuming a role."""
     for statement in policy_document.get('Statement', []):
         if statement.get('Effect') == 'Allow' and 'sts:AssumeRole' in statement.get('Action', []):
             return True
-    else:
-        print("test")
     return False
 
 def assume_user_role(session, role_name, role_arn):
@@ -140,8 +142,8 @@ def main():
     args = parse_arguments()
     session = get_session(args.profile)
     user_arn = authenticate_user(session)
-    assumable_roles = get_assumable_roles(session, user_arn)
-    role_chaning_check(session, assumable_roles)
+    permisive_roles = get_permisive_roles(session, user_arn)
+    role_chaning_check(session, permisive_roles)
 
 if __name__ == '__main__':
     main()
